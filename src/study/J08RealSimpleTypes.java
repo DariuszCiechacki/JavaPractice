0cbc09f0824eb13680704690c6f2c054216dfde0
unknown <ciechacki.dariusz@gmail.com>
2025-03-24 20:41:00 +0100

    feat: adds to 12 classes

---
+++ src/study/J08RealSimpleTypes.java
@@ -0,0 +1,11 @@
+package study;
+
+public class J08RealSimpleTypes {
+
+    public static void main(String[] args){
+        float smallNumb = 10.5f;
+        double num2b = 123.876d;
+
+        System.out.println();
+    }
+}
