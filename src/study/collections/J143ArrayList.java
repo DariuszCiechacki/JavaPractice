f030b437209c98686c93eeeec0cbc9fa1afa49b2
unknown <ciechacki.dariusz@gmail.com>
2025-07-27 14:54:33 +0200

    ArrayList

---
+++ src/study/collections/J143ArrayList.java
@@ -0,0 +1,76 @@
+package study.collections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/*
+Key features of ArrayList:
+1. Ordered (elements maintain insertion order)
+2. Allows duplicates
+3. Allows null elements
+4. Not synchronized (i.e., not thread-safe)
+5. Internally uses an Object[] array
+ */
+
+public class J143ArrayList {
+    public static void main(String[] args) {
+        //basicActions();
+        arraySorting();
+    }
+
+    private static void basicActions() {
+        ArrayList<String> fruits = new ArrayList<>();
+
+        // Adding elements
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Strawberry");
+        System.out.println(fruits); // [Apple, Banana, Strawberry]
+
+        // Access by index
+        System.out.println(fruits.get(0)); // Apple
+
+        // Update array
+        fruits.set(1, "Orange");
+        System.out.println(fruits.get(1)); // Orange
+        System.out.println(fruits); // [Apple, Orange, Strawberry]
+
+        // Remove
+        fruits.remove("Orange");
+        System.out.println(fruits); // [Apple, Strawberry]
+
+        // Check size
+        System.out.println(fruits.size()); // 2
+
+        // Check if list contains an element
+        System.out.println(fruits.contains("Apple")); // true
+        System.out.println(fruits.contains("Pineapple")); // false
+    }
+
+    private static void arraySorting() {
+        // Sorting integers
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(5421);
+        numbers.add(2);
+        numbers.add(123);
+        numbers.add(55);
+        Collections.sort(numbers);
+        System.out.println(numbers); // [2, 55, 123, 5421]
+
+        // Sorting strings
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Kasia");
+        names.add("Mariusz");
+        names.add("Wincent");
+        names.add("Karolina");
+        Collections.sort(names);
+        System.out.println(names); // [Karolina, Kasia, Mariusz, Wincent]
+
+        // Sorting in reverse order
+        Collections.sort(numbers, Collections.reverseOrder());
+        System.out.println(numbers);  // [5421, 123, 55, 2]
+
+        Collections.sort(names, Collections.reverseOrder());
+        System.out.println(names); // [Wincent, Mariusz, Kasia, Karolina]
+    }
+}
---
+++ src/study/collections/J144ArrayListChallenge.java
@@ -0,0 +1,23 @@
+package study.collections;
+
+import java.util.ArrayList;
+
+public class J144ArrayListChallenge {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Ola");
+        names.add("Kasia");
+        names.add("Daniel");
+
+        System.out.println("First name is: " + names.get(0));
+
+        names.set(2, "Olek");
+        names.add(1, "Karol");
+        names.remove(0);
+
+        System.out.println("Elementy: ");
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
